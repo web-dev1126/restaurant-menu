d8cab7c290ca88e65d36fb2002a74b504debf07c
Igor <webigor25@gmail.com>
2025-07-23 14:30:00 +0200

    feat: implement RESTful API controllers for Menu and MenuItem resources [Level 1]

---
+++ app/controllers/api/v1/menu_items_controller.rb
@@ -0,0 +1,53 @@
+class Api::V1::MenuItemsController < ApplicationController
+  before_action :set_menu_item, only: [:show, :update, :destroy]
+  before_action :set_menu, only: [:index, :create]
+
+  def index
+    @menu_items = @menu.menu_items
+    render json: @menu_items
+  end
+
+  def show
+    render json: @menu_item
+  end
+
+  def create
+    @menu_item = @menu.menu_items.build(menu_item_params)
+    if @menu_item.save
+      render json: @menu_item, status: :created
+    else
+      render json: { errors: @menu_item.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @menu_item.update(menu_item_params)
+      render json: @menu_item
+    else
+      render json: { errors: @menu_item.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @menu_item.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_menu_item
+    @menu_item = MenuItem.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: 'Menu item not found' }, status: :not_found
+  end
+
+  def set_menu
+    @menu = Menu.find(params[:menu_id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: 'Menu not found' }, status: :not_found
+  end
+
+  def menu_item_params
+    params.require(:menu_item).permit(:name, :description, :price, :category, :available)
+  end
+end 
---
+++ app/controllers/api/v1/menus_controller.rb
@@ -0,0 +1,46 @@
+class Api::V1::MenusController < ApplicationController
+  before_action :set_menu, only: [:show, :update, :destroy]
+
+  def index
+    @menus = Menu.all
+    render json: @menus
+  end
+
+  def show
+    render json: @menu
+  end
+
+  def create
+    @menu = Menu.new(menu_params)
+    if @menu.save
+      render json: @menu, status: :created
+    else
+      render json: { errors: @menu.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @menu.update(menu_params)
+      render json: @menu
+    else
+      render json: { errors: @menu.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @menu.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_menu
+    @menu = Menu.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: 'Menu not found' }, status: :not_found
+  end
+
+  def menu_params
+    params.require(:menu).permit(:name, :description, :active)
+  end
+end 
